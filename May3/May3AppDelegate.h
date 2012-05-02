c65df1216e318cb2c41dfc0e418ddb07ad23c50b
Sunil Gopinath <sxg282@gmail.com>
2012-05-02 19:53:13 -0400

    first commit

---
+++ May3/May3AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  May3AppDelegate.h
+//  May3
+//
+//  Created by Sunil Gopinath on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface May3AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
